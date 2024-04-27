97de9c0cd956486e01aaa00949b8b7042292a332
USER <USER@USERdeMacBook-Pro.local>
2024-04-27 15:33:50 +0800

    Initial Commit

--- coffee01.c
+++ coffee01.c
@@ -1,1 +1,7 @@
 Hello there
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/allofmeng/TestRepo01.git
+git push -u origin main      git add --all
